c2d8b98b954bf16654f8147d56d0b13eade1d25a
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2023-01-12 16:24:26 +0100

    e

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-//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
-
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
   organization: "org-i2zCpP7gWEGozTOZikoO9mf3",
-  apiKey: "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",
+  apiKey: "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",
 });
-const openai = new OpenAIAPI(configuration);
-
-async function callApi(){
-
-
-const response = await openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: "Say this is a test",
-  max_tokens: 7,
-  temperature: 0,
-});
-console.log(response.data.choices[0].text)
+const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
+async function callApi() {
+  const response = await openai.createCompletion({
+    "model": "text-davinci-003",
+    "prompt": "Say this is a test",
+    "max_tokens": 7,
+    "temperature": 0
+  });
+  console.log(response.data.choices[0].text)
 }
 callApi()
